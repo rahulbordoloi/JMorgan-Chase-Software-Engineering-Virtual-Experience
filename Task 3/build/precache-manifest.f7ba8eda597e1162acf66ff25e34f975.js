73d6636043c4d6ef3a0a22bca9d9444097bad3a3
Rahul Bordoloi <rahulbordoloi24@gmail.com>
2020-05-09 17:34:53 +0530

    build

---
+++ Task 3/build/precache-manifest.f7ba8eda597e1162acf66ff25e34f975.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1b7d3e80fdf3e94a5b11",
+    "url": "/static/css/main.bf120cf0.chunk.css"
+  },
+  {
+    "revision": "1b7d3e80fdf3e94a5b11",
+    "url": "/static/js/main.1b7d3e80.chunk.js"
+  },
+  {
+    "revision": "fc5daf24e578255d515e",
+    "url": "/static/css/1.626ce596.chunk.css"
+  },
+  {
+    "revision": "fc5daf24e578255d515e",
+    "url": "/static/js/1.fc5daf24.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c948e8278e5a08766f3a2162fd41ac27",
+    "url": "/index.html"
+  }
+];
