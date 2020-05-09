08e462b29138362baa67b0270025643c70ddbcc5
Rahul Bordoloi <rahulbordoloi24@gmail.com>
2020-05-09 10:31:43 +0530

    Build.

---
+++ Task 2/build/precache-manifest.d5ed4cde6f816beae09e34f484d1f668.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5cf718a5217b5bd6e4899f41e7d7923",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c6f216d6085ce249750d",
+    "url": "/static/css/2.d6500151.chunk.css"
+  },
+  {
+    "revision": "c2ae6d3cebf3d7db983e",
+    "url": "/static/css/main.84040203.chunk.css"
+  },
+  {
+    "revision": "c6f216d6085ce249750d",
+    "url": "/static/js/2.99fb6d3d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.99fb6d3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2ae6d3cebf3d7db983e",
+    "url": "/static/js/main.6dcfe521.chunk.js"
+  },
+  {
+    "revision": "1cfedb3388fe4a66ebb3",
+    "url": "/static/js/runtime-main.ae63fd09.js"
+  }
+]);
